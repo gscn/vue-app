c75adf354f3f02b9a8e0c6081620b84661637a86
redray <redraycn@gmail.com>
2023-10-21 15:04:47 +0800

    清理无用文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="index">
+    <h1>This is an index page</h1>
+  </div>
 </template>
